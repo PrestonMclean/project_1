a4ee35d2b3b26c13fd02d468470f6e06c86c7684
PrestonMclean <mclean_preston@hotmail.com>
2021-01-08 14:53:15 -0600

    change index
    change index file to a php file.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Random Quotes</title>
+  <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="css/styles.php">
+</head>
+<body>
+  <div class="container">
+    <div id="quote-box">
+
+  </div>
+</body>
+</html>
